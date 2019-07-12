06fb82196591cb5509df17871a0e0e408a66de4d
Arli <arli.machado@gmail.com>
2019-07-12 10:28:42 -0300

    Incluindo testes de autenticacao

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,79 @@
+const request = require("supertest");
+
+const app = require("../../src/app");
+const truncate = require("../utils/truncate");
+const { User } = require("../../src/app/models");
+
+describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it("should be able to authenticate with valid credentials", async () => {
+    const user = await User.create({
+      name: "Arli",
+      email: "arli@mail.com",
+      password: "123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: "arli@mail.com",
+        password: "123"
+      });
+    expect(response.status).toBe(200);
+  });
+
+  it("should not be able to authenticate with invalid credentials", async () => {
+    const user = await User.create({
+      name: "Arli",
+      email: "arli@mail.com",
+      password: "123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: "arli@mail.com",
+        password: "12345"
+      });
+    expect(response.status).toBe(401);
+  });
+
+  it("should return jwt token when authenticated", async () => {
+    const user = await User.create({
+      name: "Arli",
+      email: "arli@mail.com",
+      password: "123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: "arli@mail.com",
+        password: "123"
+      });
+    expect(response.body).toHaveProperty("token");
+  });
+
+  it("should be able to access private routes when authenticated", async () => {
+    const user = await User.create({
+      name: "Arli",
+      email: "arli@mail.com",
+      password: "123"
+    });
+
+    const response = await request(app)
+      .get("/dashboard")
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should not be able to access private routes when not authenticate", async () => {
+    const response = await request(app).get("/dashboard");
+
+    expect(response.status).toBe(401);
+  });
+});
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,12 @@
 const routes = require("express").Router();
 
 const { User } = require("./app/models");
+const SessionController = require("./app/controllers/SessionController");
 
-routes.get("/", async (req, res) => {
-  const user = await User.create({
-    name: "Arli",
-    email: "arli@mail.com",
-    password_hash: "123"
-  });
-  return res.json({ user });
+routes.post("/sessions", SessionController.store);
+
+routes.get("/dashboard", (req, res) => {
+  return res.status(200).send();
 });
 
 module.exports = routes;
