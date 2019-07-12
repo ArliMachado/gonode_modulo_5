dde43f2fffedff8003a39f8fee79f942f1714982
Arli <arli.machado@gmail.com>
2019-07-12 14:14:23 -0300

    Refatorando testes

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 
 const app = require("../../src/app");
 const truncate = require("../utils/truncate");
-const { User } = require("../../src/app/models");
+const factory = require("../factories");
 
 describe("Authentication", () => {
   beforeEach(async () => {
@@ -10,59 +10,49 @@ describe("Authentication", () => {
   });
 
   it("should be able to authenticate with valid credentials", async () => {
-    const user = await User.create({
-      name: "Arli",
-      email: "arli@mail.com",
+    const user = await factory.create("User", {
       password: "123"
     });
 
     const response = await request(app)
       .post("/sessions")
       .send({
-        email: "arli@mail.com",
+        email: user.email,
         password: "123"
       });
     expect(response.status).toBe(200);
   });
 
   it("should not be able to authenticate with invalid credentials", async () => {
-    const user = await User.create({
-      name: "Arli",
-      email: "arli@mail.com",
+    const user = await factory.create("User", {
       password: "123"
     });
 
     const response = await request(app)
       .post("/sessions")
       .send({
-        email: "arli@mail.com",
+        email: user.email,
         password: "12345"
       });
     expect(response.status).toBe(401);
   });
 
   it("should return jwt token when authenticated", async () => {
-    const user = await User.create({
-      name: "Arli",
-      email: "arli@mail.com",
+    const user = await factory.create("User", {
       password: "123"
     });
 
     const response = await request(app)
       .post("/sessions")
       .send({
-        email: "arli@mail.com",
+        email: user.email,
         password: "123"
       });
     expect(response.body).toHaveProperty("token");
   });
 
   it("should be able to access private routes when authenticated", async () => {
-    const user = await User.create({
-      name: "Arli",
-      email: "arli@mail.com",
-      password: "123"
-    });
+    const user = await factory.create("User");
 
     const response = await request(app)
       .get("/dashboard")
